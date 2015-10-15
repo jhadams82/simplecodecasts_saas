02d2ae1a88704328830e3ee42fd9277658e3c2cf
Justin Adams <jhadams82@gmail.com>
2015-10-15 01:21:07 +0000

    Added index action and updated link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,10 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
-    before_action :only_current_user
+    
+    def index
+    end
     
     def show
         @user = User.find( params[:id] ) 
-    end
-    
-    private
-    def only_current_user
-        @user = User.find( params[:id] )
-        redirect_to(root_url) unless @user == current_user
     end
 end
